b5f16dd1a09fd1f463f0c1518c64cfd5ed92f80c
jinseok1006 <jinseok1006@naver.com>
2023-01-18 03:08:42 +0900

    first

---
+++ src/parse.js
@@ -0,0 +1,28 @@
+const cheerio = require('cheerio');
+const fs = require('fs/promises');
+const path = require('path');
+const types = require('./types.js');
+const keys = Object.keys(types);
+
+async function parse(src) {
+  const xmlFile = await fs.readFile(`${src}.xml`);
+  const $ = cheerio.load(xmlFile, { xmlMode: true }, false);
+
+  const data = $('Row')
+    .map((i, el) =>
+      keys.reduce(
+        (pre, key) => ({
+          ...pre,
+          [types[key].id]: $(el).find(`Col[id=${key}]`).text().trim(),
+        }),
+        {}
+      )
+    )
+    .get();
+
+  const writeSrc = `${src}.json`;
+  fs.writeFile(writeSrc, JSON.stringify(data, null, 4));
+}
+
+const xmlSrc = path.resolve(__dirname, './assets/lect2');
+parse(xmlSrc);
